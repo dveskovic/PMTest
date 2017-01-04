7a619e50005b9a84b8320c4ef4427e3a6928591a
n2go <n2go@logeecom.com>
2017-01-04 14:18:47 +0100

    commit30

--- src/Controllers/DataController.php
+++ src/Controllers/DataController.php
@@ -13,7 +13,7 @@ use Plenty\Plugin\Http\Response;
 use Plenty\Plugin\Http\Request;
 use IO\Services\ItemService;
 use Plenty\Modules\Plugin\Storage\Contracts;
-use Illuminate\Filesystem;
+use www3\plenty\stable7_b\pl\vendor\laravel\framework\src\Illuminate\Filesystem;
 
 
 /**
@@ -101,7 +101,7 @@ class DataController extends Controller
        // $this->storage->uploadFile('pmtest', $fileContent, $file, true, null);
 
         //Example
-        $this->file->put($directory,'Test18');
+        $this->file->put($directory,'Test19');
 
 
         $test = ['test' => $directory];
