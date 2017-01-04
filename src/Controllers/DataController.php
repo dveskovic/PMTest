8d3cde7e1221c4ca00e8b57a91d5325d4b4a5bb3
n2go <n2go@logeecom.com>
2017-01-04 15:11:08 +0100

    commit35

--- src/Controllers/DataController.php
+++ src/Controllers/DataController.php
@@ -101,7 +101,7 @@ class DataController extends Controller
        // $this->storage->uploadFile('pmtest', $fileContent, $file, true, null);
 
         //Example
-        $this->file->put($directory,'Test18.json');
+        $this->file->put($directory,'Test21.json');
 
 
         $test = ['test' => $directory];
