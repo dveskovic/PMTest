b906f2e0e1a03b5e080fe3020f0ff916aa702c1c
n2go <n2go@logeecom.com>
2017-01-17 18:22:58 +0100

    com1

--- src/Controllers/ProductsController.php
+++ src/Controllers/ProductsController.php
@@ -73,8 +73,8 @@ class ProductsController extends Controller
             ];
         }*/
 
-        $params = (object) ['itemsPerPage' => 3];
-        $product = $this->itemService->searchItems('', $params, 7);
+       // $params = (object) ['itemsPerPage' => 3];
+       // $product = $this->itemService->searchItems('', $params, 7);
       //  return $this->response->json($products);
         echo 'tu saaaam';
     }
