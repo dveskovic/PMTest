44a01cb862e61ffd8c5aee7dc783af856706f563
n2go <n2go@logeecom.com>
2017-01-27 11:51:29 +0100

    Revert "Removed files"
    This reverts commit 9b14ee459ea3fe1e568819eb451b0b439113a1f4.

---
+++ src/Controllers/SettingsController.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace PMTest\Controllers;
+
+use PMTest\Services\SettingsService;
+use PMTest\Helpers\Data;
+use Plenty\Plugin\Controller;
+use Plenty\Plugin\Http\Request;
+use Plenty\Modules\Helper\Services\WebstoreHelper;
+use Plenty\Plugin\Http\Response;
+
+class SettingsController extends Controller
+{
+    const YOOCHOOSE_LICENSE_URL = 'https://admin.yoochoose.net/api/v4/';
+
+    /**
+     * @var SettingsService
+     */
+    private $settingsService;
+
+    /**
+     * @var Data
+     */
+    private $helper;
+
+    /**
+     * @var WebstoreHelper
+     */
+    private $storeHelper;
+
+    /**
+     * @var Response
+     */
+    private $response;
+
+    /**
+     * SettingsController constructor.
+     * @param SettingsService $settingsService
+     * @param Data $helper
+     * @param WebstoreHelper $storeHelper
+     * @param Response $response
+     */
+    public function __construct
+    (
+        SettingsService $settingsService,
+        Data $helper,
+        WebstoreHelper $storeHelper,
+        Response $response
+    ) {
+        $this->settingsService = $settingsService;
+        $this->helper = $helper;
+        $this->storeHelper = $storeHelper;
+        $this->response = $response;
+    }
+
+    /**
+     * @param Request $request
+     * @return \Plenty\Plugin\Http\Response;
+     * @throws \Exception
+     */
+    public function saveSettings(Request $request)
+    {
+        $endpoint = $this->settingsService->getSettingsValue('endpoint');
+        $configFields = [];
+
+        /** @var \Plenty\Modules\System\Models\WebstoreConfiguration $webstoreConfig */
+        $webstoreConfig = $this->storeHelper->getCurrentWebstoreConfiguration();
+        if (is_null($webstoreConfig)) {
+            return $this->response->json('error');
+        }
+        $baseURL = $webstoreConfig->domain;
+
+        $configFields['customer_id'] = $request->get('customer_id');
+        $configFields['license_key'] = $request->get('license_key');
+        $configFields['plugin_id'] = $request->get('plugin_id');
+        $configFields['design'] = $request->get('design');
+        $configFields['item_type'] = $request->get('item_type');
+        $configFields['script_id'] = $request->get('script_id');
+        $configFields['search_enable'] = $request->get('search_enable');
+        $configFields['performance'] = $request->get('performance');
+        $configFields['log_severity'] = $request->get('log_severity');
+//        $configFields['auth_token'] = $request->get('auth_token');
+
+//        if (!$configFields['auth_token']) {
+//            return $this->response->json('Reset current Authorization token because token must not be empty!');
+//        }
+
+        if (!$endpoint || $endpoint != $baseURL) {
+            $configFields['endpoint'] = $baseURL;
+        }
+
+
+
+        foreach ($configFields as $key => $value) {
+            if(!empty($value)) {
+                switch ($key) {
+                    case 'customer_id':
+                        $this->settingsService->setSettingsValue('customer_id', $value);
+                        break;
+                    case 'license_key':
+                        $this->settingsService->setSettingsValue('license_key', $value);
+                        break;
+                    case 'plugin_id':
+                        $this->settingsService->setSettingsValue('plugin_id', $value);
+                        break;
+                    case 'design':
+                        $this->settingsService->setSettingsValue('design', $value);
+                        break;
+                    case 'item_type':
+                        $this->settingsService->setSettingsValue('item_type', $value);
+                        break;
+                    case 'script_id':
+                        $this->settingsService->setSettingsValue('script_id', $value);
+                        break;
+                    case 'search_enable':
+                        $this->settingsService->setSettingsValue('search_enable', $value);
+                        break;
+                    case 'performance':
+                        $this->settingsService->setSettingsValue('performance', $value);
+                        break;
+                    case 'log_severity':
+                        $this->settingsService->setSettingsValue('log_severity', $value);
+                        break;
+//                    case 'auth_token':
+//                        $this->settingsService->setSettingsValue('auth_token', $value);
+//                        break;
+                    case 'endpoint':
+                        $this->settingsService->setSettingsValue('endpoint', $value);
+                        break;
+                }
+            }
+        }
+
+
+        $customerId = $this->settingsService->getSettingsValue('customer_id');
+        $licenseKey = $this->settingsService->getSettingsValue('license_key');
+
+        $body = [
+            'base' => [
+                'type' => "MAGENTO2",
+                'pluginId' => $this->settingsService->getSettingsValue('plugin_id'),
+                'endpoint' => $baseURL,
+                'appKey' => '',
+                'appSecret' => '',
+            ],
+            'frontend' => [
+                'design' => $this->settingsService->getSettingsValue('design'),
+            ],
+            'search' => [
+                'design' => $this->settingsService->getSettingsValue('design'),
+            ],
+        ];
+
+        $url = self::YOOCHOOSE_LICENSE_URL . $customerId . '/plugin/update?createIfNeeded=true&fallbackDesign=true';
+        $response = $this->helper->getHttpPage($url, $body, $customerId, $licenseKey);
+        $resault = json_decode($response);
+
+        if ($resault['statusCode'] == 200) {
+            return $this->response->json('User verification successful');
+        } else {
+            return $this->response->json('User is not verified!');
+        }
+    }
+
+    /**
+     * @return bool|mixed
+     */
+    public function loadSettings()
+    {
+        $data = array(
+            'customer_id' => $this->settingsService->getSettingsValue('customer_id'),
+            'license_key' => $this->settingsService->getSettingsValue('license_key'),
+            'plugin_id' => $this->settingsService->getSettingsValue('plugin_id'),
+            'design' => $this->settingsService->getSettingsValue('design'),
+            'item_type' => $this->settingsService->getSettingsValue('item_type'),
+            'script_id' => $this->settingsService->getSettingsValue('script_id'),
+            'search_enable' => $this->settingsService->getSettingsValue('search_enable'),
+            'performance' => $this->settingsService->getSettingsValue('performance'),
+            'log_severity' => $this->settingsService->getSettingsValue('log_severity'),
+//            'auth_token' => $this->settingsService->getSettingsValue('auth_token'),
+            'endpoint' => $this->settingsService->getSettingsValue('endpoint'),
+        );
+        return $this->response->json($data);
+    }
+
+}
--- src/Providers/PMTestRouteServiceProvider.php
+++ src/Providers/PMTestRouteServiceProvider.php
@@ -15,8 +15,13 @@ class PMTestRouteServiceProvider extends RouteServiceProvider
 	 */
 	public function map(Router $router)
 	{
-        $router->get('customers', 'PMTest\Controllers\CustomersController@customers');
+		$router->get('hello', 'PMTest\Controllers\ContentController@sayHello');
+		$router->get('yc/export', 'PMTest\Controllers\ExportController@export');
+		$router->get('customers', 'PMTest\Controllers\CustomersController@customers');
 
+		//settings
+		$router->post('pmtest/settings/', 'PMTest\Controllers\SettingsController@saveSettings');
+		$router->get('pmtest/settings/', 'PMTest\Controllers\SettingsController@loadSettings');
 	}
 
 }
