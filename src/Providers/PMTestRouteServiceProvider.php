8a974958217ca3135e975c869cadd6bc281a22df
n2go <n2go@logeecom.com>
2017-01-27 11:53:05 +0100

    Revert "Revert "Removed files""
    This reverts commit 44a01cb862e61ffd8c5aee7dc783af856706f563.

--- src/Providers/PMTestRouteServiceProvider.php
+++ src/Providers/PMTestRouteServiceProvider.php
@@ -15,13 +15,8 @@ class PMTestRouteServiceProvider extends RouteServiceProvider
 	 */
 	public function map(Router $router)
 	{
-		$router->get('hello', 'PMTest\Controllers\ContentController@sayHello');
-		$router->get('yc/export', 'PMTest\Controllers\ExportController@export');
-		$router->get('customers', 'PMTest\Controllers\CustomersController@customers');
+        $router->get('customers', 'PMTest\Controllers\CustomersController@customers');
 
-		//settings
-		$router->post('pmtest/settings/', 'PMTest\Controllers\SettingsController@saveSettings');
-		$router->get('pmtest/settings/', 'PMTest\Controllers\SettingsController@loadSettings');
 	}
 
 }
