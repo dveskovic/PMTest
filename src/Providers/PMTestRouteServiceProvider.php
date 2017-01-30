f698d6ca6be836df68008a90ebf7f345b23acb6f
n2go <n2go@logeecom.com>
2017-01-30 14:41:32 +0100

    dk25

--- src/Providers/PMTestRouteServiceProvider.php
+++ src/Providers/PMTestRouteServiceProvider.php
@@ -15,7 +15,7 @@ class PMTestRouteServiceProvider extends RouteServiceProvider
 	 */
 	public function map(Router $router)
 	{
-		$router->get('n2go/customers', 'PMTest\Controllers\CustomersController@customers');
+		$router->get('n2goo/customers', 'PMTest\Controllers\CustomersController@customers');
 	}
 
 }
