f9b2f9450f70ad2cbca5da3875dbdb0598d44c22
n2go <n2go@logeecom.com>
2017-02-01 15:22:03 +0100

    h29

--- src/Controllers/CustomersController.php
+++ src/Controllers/CustomersController.php
@@ -48,90 +48,9 @@ class CustomersController extends Controller
      */
     public function customers()
     {
-        $result = '';
-        $param = '';
-        $group = $this->request->get('group');
-        $subscribed = $this->request->get('subscribed');
-        $emails = $this->request->get('emails');
-        $fields = $this->request->get('fields');
-        $emails = json_decode($emails, true);
-        $fields = json_decode($fields, true);
+      $data = 'tu sam';
 
-
-        if (isset($group) && isset($subscribed) == false) {
-            $param = 1;
-        }
-        if (isset($group) && isset($subscribed) && $subscribed == 'true') {
-            $param = 2;
-        }
-        if (isset($group) && isset($subscribed) && $subscribed == 'false') {
-            $param = 3;
-        }
-        if (isset($subscribed) && isset($group) == false && $subscribed == 'true') {
-            $param = 4;
-        }
-        if (isset($subscribed) && isset($group) == false && $subscribed == 'false') {
-            $param = 5;
-        }
-        if (isset($group) && isset($emails)) {
-            $param = 6;
-        }
-        
-        $accounts = $this->account->allAccounts();
-        foreach ($accounts as $a) {
-            $contacts = $this->account->getContactsOfAccount($a->id);
-            foreach ($contacts as $contact) {
-                $contact['companyName'] = $a->companyName;
-                $contact['taxIdNumber'] = $a->taxIdNumber;
-
-                switch ($param) {
-                    case 1:
-                        if ($contact['typeId'] == $group) {
-                            $result[] = $contact;
-                        }
-                        break;
-                    case 2:
-                        if ($contact['typeId'] == $group && $contact['newsletterAllowanceAt'] != null) {
-                            $result[] = $contact;
-                        }
-                        break;
-                    case 3:
-                        if ($contact['typeId'] == $group) {
-                            $result[] = $contact;
-                        }
-                        break;
-                    case 4:
-                        if ($contact['newsletterAllowanceAt'] != null) {
-                            $result[] = $contact;
-                        }
-                        break;
-                    case 5: {
-                        $result[] = $contact;
-                    }
-                        break;
-                    case 6: {
-                        if ($contact['typeId'] == $group) {
-
-                            foreach ($contact['options'] as $option) {
-                                foreach ($emails as $email) {
-
-                                    if ($option['typeId'] == 2 && $option['subTypeId'] == 4 && $option['value'] == $email) {
-                                        $result[] = $contact;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                        break;
-                 
-                    default:
-                        $result[] = $contact;
-                        break;
-                }
-            }
-        }
-
-        return $this->response->json($result);
+      return $data;
     }
 
 }
